0ede13d85bfa8136960a71a052a5bdf438c40bc6
Adam <inmon.adam@yahoo.com>
2022-02-02 15:30:44 -0600

    commit tree 7cc89471beb375d78312facf5cebe5faf5cfc2b8

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 class Person
-  # your code here
+  
+  
+  def initialize(user_hash)
+    user_hash.each do |key, value|
+      # binding.pry
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
 end
